9bf30dc880b5ccb014d3650bdd3f127d3d0467eb
Marjorie Santana <marjorie.santana@growit.com.do>
2021-08-04 15:50:26 -0400

    [ADD] app mrp_bom_variable_quantity

---
+++ mrp_bom_variable_quantity/__manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': 'Variable quantity in production',
+    'version': '13.0.3.0.0',
+    'license': 'AGPL-3',
+    'author': "OCA, BADEP",
+    'website': 'https://badep.ma',
+    'depends': ['mrp'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/mrp_bom_line_formula_view.xml',
+        'views/mrp_bom_view.xml',
+    ],
+    'installable': True,
+}
